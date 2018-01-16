04a1c12db404a4e5edf29d45fd6e143cb9a8e058
edwin rivera <waehake@gmail.com>
2018-01-16 17:27:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ global.expect = require('expect');
 const babel = require('babel-core');
 const jsdom = require('jsdom');
 const path = require('path');
+
+function shout(phrase) {
+  return phrase.toUpperCase();
+}
